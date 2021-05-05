10f15ae301de98b41c3266b72d9a61a7232e1652
peterbchi <peter.chi@villanova.edu>
2021-05-05 10:32:06 -0400

    initial commit

---
+++ R/Genedupdip_neutralgenerator.R
@@ -0,0 +1,160 @@
+library(Matrix)
+
+Genedupdip_neutralgenerator <- function(N,up){
+  #Generator for the model without neofunctionalization
+  Pos <- matrix(0, ncol=N+1, nrow=N+1)
+  count <- 0
+  
+  
+  #Create indexing matrix Pos such that Q(Pos(i,j),:) is row of Q
+  #corresponding to state (i,j), etc.
+  for (i in 1:(N+1)){
+    for (j in 1:(N+2-i)){
+      #if ~(i==1 && j ==1) #&& ~(i== 1 && j == N+1)
+      count <- count + 1
+      Pos[i,j] <- count
+    }
+  }
+  
+  
+  # Evaluate number of non-zero entries of Q
+  nonzerolength <- 7*(N-2)*(N-1)/2 + (N-1)*15 + 5
+
+    
+  #Declare index vectors ii, jj and rate vector vv for construction of sparse
+  #Q matrix (ii(n) = i, jj(n) = j, vv(n) = q_ij <-> Q(i,j) = q_ij)
+  ii <- rep(0, nonzerolength)
+  jj <- rep(0, nonzerolength)
+  vv <- rep(0, nonzerolength)
+  
+  #i is number of AAAA
+  #j is number of AAA-
+  #k = N-i-j is number of AA--
+  
+  #First consider 'middle transitions' when nothing is 0
+  count1 <- 1
+  for (i in 2:N){#1:N-1, +1 for indexing
+    for (j in 2:(N-i+1)){#similar
+      if((N-i+1) > 1){
+        k <- N-(i-1)-(j-1)#k not indexing so no need to modify
+        # changed pbi, pbj, pbk to a single pb vector
+        pb <- pbirth(i-1,j-1,k,N)
+        
+        pdi <- (i-1)/N 
+        pdj <- (j-1)/N 
+        pdk <- k/N
+        
+        ii[count1:(count1+6)] <- Pos[i,j]
+        jj[count1:(count1+6)] <- c(Pos[i+1,j],Pos[i+1,j-1],Pos[i-1,j+1],Pos[i,j+1],Pos[i-1,j],Pos[i,j-1],Pos[i,j])
+        vv[count1]   <- pdk*pb[1]
+        vv[count1+1] <- pdj*pb[1]
+        vv[count1+2] <- pdi*pb[2]+2*(i-1)*up
+        vv[count1+3] <- pdk*pb[2]
+        vv[count1+4] <- pdi*pb[3]
+        vv[count1+5] <- pdj*pb[3]+(j-1)*up
+        vv[count1+6] <- -sum(vv[count1:(count1+5)])
+        count1 <- count1+7
+      }
+    }
+    #Now transitions where k = 0
+    j <- N-i+2
+    k <- N-(i-1)-(j-1)#=0
+    if (k != 0){
+      stop("k should be 0 but isn't")
+    }
+    pb <- pbirth(i-1,j-1,k,N)
+    pdi <- (i-1)/N
+    pdj <- (j-1)/N
+    pdk <- k/N
+    
+    ii[count1:(count1+4)] <- Pos[i,j] 
+    jj[count1:(count1+4)] <- c(Pos[i+1,j-1],Pos[i-1,j+1],Pos[i,j-1],Pos[i-1,j],Pos[i,j])
+    vv[count1] <- pdj*pb[1]
+    vv[count1+1] <- pdi*pb[2]+2*(i-1)*up
+    vv[count1+2] <- pdj*pb[3]+(j-1)*up
+    vv[count1+3] <- pdi*pb[3]
+    vv[count1+4] <- -sum(vv[count1:(count1+3)])
+    count1 <- count1+5
+  }
+  #Now transitions where i = 0
+  i <- 1#indexing
+  for (j in 2:N){
+    k <- N-(i-1)-(j-1)
+    pb <- pbirth(i-1,j-1,k,N)
+    
+    pdi <- (i-1)/N
+    pdj <- (j-1)/N
+    pdk <- k/N
+    
+    ii[count1:(count1+4)] <- Pos[i,j]
+    jj[count1:(count1+4)] <- c(Pos[i,j+1],Pos[i,j-1],Pos[i+1,j-1],Pos[i+1,j],Pos[i,j])
+    vv[count1] <- pdk*pb[2]
+    vv[count1+1] <- pdj*pb[3]+(j-1)*up
+    vv[count1+2] <- pdj*pb[1]
+    vv[count1+3] <- pdk*pb[1]
+    vv[count1+4] <- -sum(vv[count1:(count1+3)])
+    count1 <- count1 + 5
+  }
+  #Now transitions where i = N;
+  i <- N+1
+  j <- 1
+  k <- 0
+  #only pseudogenization here
+  ii[count1:(count1+1)] <- Pos[i,j]
+  jj[count1:(count1+1)] <- c(Pos[i-1,j+1],Pos[i,j])
+  vv[count1] <- 2*(i-1)*up
+  vv[count1+1] <- -vv[count1]
+  count1 <- count1+2
+  
+  #Now transitions where j = 0;
+  j <- 1
+  for (i in 2:N){
+    k <- N-(i-1)-(j-1)
+    pb <- pbirth(i-1,j-1,k,N)
+    pdi <- (i-1)/N
+    pdj <- (j-1)/N
+    pdk <- k/N
+    
+    ii[count1:(count1+4)] <- Pos[i,j]
+    jj[count1:(count1+4)] <- c(Pos[i+1,j],Pos[i-1,j],Pos[i,j+1],Pos[i-1,j+1],Pos[i,j])
+    vv[count1] <- pdk*pb[1]
+    vv[count1+1] <- pdi*pb[3]
+    vv[count1+2] <- pdk*pb[2]
+    vv[count1+3] <- pdi*pb[2]+2*(i-1)*up
+    vv[count1+4] <- -sum(vv[count1:(count1+3)])
+    count1 <- count1+5
+  }
+  
+  #Now transitions where j = N;
+  j <- N+1
+  i <- 1
+  k <- 0
+  pb <- pbirth(i-1,j-1,k,N)#j's can give birth to both other types so we need these, obviously pdj = 1 so that's omitted.
+  ii[count1:(count1+2)] <- Pos[i,j]
+  jj[count1:(count1+2)] <- c(Pos[i+1,j-1],Pos[i,j-1],Pos[i,j])
+  vv[count1] <- pb[1]
+  vv[count1+1] <- pb[3] +(j-1)*up
+  vv[count1+2] <- -vv[count1]-vv[count1+1]
+  count1 <- count1+2
+  
+  #Finished getting rates
+  
+  # Q is the generator matrix
+  Q <- sparseMatrix(i=ii,j=jj,x=vv,dims=c(count,count), symmetric=FALSE)
+  #e1 is initial distribution
+  e1 <- sparseMatrix(i=1,j=Pos[2,1],x=1,dims=c(1,count), symmetric=FALSE)
+  return(list(Q, e1, Pos))
+#  return(list(ii, jj, vv, count)) 
+  
+}
+
+pbirth <- function(i,j,k,N){
+    #This function gives the probability of birth of type i, j, k
+    #individuals given state of population
+    pbi <- (i/N)*(i-1)/(N-1) + (1/4)*(j/N)*(j-1)/(N-1) + (j/N)*i/(N-1)
+    pbj <- (j/N)*(k/(N-1)) + (j/N)*(i/(N-1)) + (1/2)*(j/N)*((j-1)/(N-1)) +2*(i/N)*(k/(N-1))
+    pbk <- (k/N)*((k-1)/(N-1)) + (j/N)*(k/(N-1)) + (1/4)*(j/N)*((j-1)/(N-1))
+    # eps <- 1e-15 I don't know wtf this is for
+    return(c(pbi, pbj, pbk))
+}
+
---
+++ R/mexpv_dipneut.R
@@ -0,0 +1,268 @@
+#This function modifies expokit's mexpv to efficiently deal with the
+#specific problem of calculating proportion of duplicated genes in haploid
+#population model.
+###########################################################################
+
+#  [w, err, hump] = m  expv( t, A, v, tol, m )
+#  MEXPV computes an approximation of w = exp(t*A)*v using Krylov
+#  subspace projection techniques. This is a customised version for
+#  Markov Chains. This means that a check is done within this code to
+#  ensure that the resulting vector w is a probability vector, i.e.,
+#  w must have all its components in [0,1], with sum equal to 1.
+#  This check is done at some expense and the user may try EXPV
+#  which is cheaper since it ignores probability constraints.
+#
+#  IMPORTANT: The check assumes that the transition rate matrix Q
+#             satisfies Qe = 0, where e = (1,...,1)'. Don't use MEXPV
+#             if this condition does not hold. Use EXPV instead.
+#             MEXPV/EXPV require A = Q', i.e., the TRANSPOSE of Q.
+#             Failure to remember this leads to wrong results.
+#
+#
+#  MEXPV does not compute the matrix exponential in isolation but
+#  instead, it computes directly the action of the exponential operator
+#  on the operand vector. This way of doing so allows for addressing
+#  large sparse problems. The matrix under consideration interacts only
+#  via matrix-vector products (matrix-free method).
+#
+#  w = mexpv( t, A, v )
+#  computes w = exp(t*A)*v using a default tol = 1.0e-7 and m = 30.
+#
+#  [w, err] = mexpv( t, A, v )
+#  renders an estimate of the error on the approximation.
+#
+#  [w, err] = mexpv( t, A, v, tol )
+#  overrides default tolerance.
+#
+#  [w, err] = mexpv( t, A, v, tol, m )
+#  overrides default tolerance and dimension of the Krylov subspace.
+#
+#  [w, err, hump] = expv( t, A, v, tol, m )
+#  overrides default tolerance and dimension of the Krylov subspace,
+#  and renders an approximation of the `hump'.
+#
+#  The hump is defined as:
+#          hump = max||exp(sA)||, s in [0,t]  (or s in [t,0] if t < 0).
+#  It is used as a measure of the conditioning of the matrix exponential
+#  problem. The matrix exponential is well-conditioned if hump = 1,
+#  whereas it is poorly-conditioned if hump >> 1.  However the solution
+#  can still be relatively fairly accurate even when the hump is large
+#  (the hump is an upper bound), especially when the hump and
+#  ||w(t)||/||v|| are of the same order of magnitude (further details in
+#  reference below). Markov chains are usually well-conditioned problems.
+#
+#  Example:
+#  --------
+#    # generate a transition rate matrix
+#    n = 100;
+#    A = rand(n);
+#    for j = 1:n
+#	 sumj = 0;
+#        for i = 1:n
+#            if rand < 0.5, A(i,j) = 0; end;
+#            sumj = sumj + A(i,j);
+#        end;
+#	 A(j,j) = A(j,j)-sumj;
+#    end;
+#    v = eye(n,1);
+#    A = sparse(A); # invaluable for a large and sparse matrix.
+#
+#    tic
+#    [w,err] = expv(1,A,v);
+#    toc
+#
+#    disp('w(1:10) ='); disp(w(1:10));
+#    disp('err =');     disp(err);
+#
+#    tic
+#    w_matlab = expm(full(A))*v;
+#    toc
+#
+#    disp('w_matlab(1:10) ='); disp(w_matlab(1:10));
+#    gap = norm(w-w_matlab)/norm(w_matlab);
+#    disp('||w-w_matlab|| / ||w_matlab|| ='); disp(gap);
+#
+#  In the above example, n could have been set to a larger value,
+#  but the computation of w_matlab will be too long (feel free to
+#  discard this computation).
+#
+#  See also EXPV, EXPOKIT.
+
+#  Roger B. Sidje (rbs@maths.uq.edu.au)
+#  EXPOKIT: Software Package for Computing Matrix Exponentials.
+#  ACM - Transactions On Mathematical Software, 24(1):130-156, 1998
+
+############################################################################
+#Tristan L. Stark - Have modified the expokit code to calculate measures
+#specific to the model under study
+
+mexpv_dipneut <- function(t, A, v,N,Pos, tol=1e-7, m=NULL){
+  #TLS Have to modify number of arguments compared to standard expokit implementation to
+  #account for extra variables N,Pos.
+  n <- dim(A)[1]
+  if(is.null(m)){
+    m <- min(n,30)
+  }
+  
+  anorm <- norm(A,'I')
+  mxrej <- 10;  btol  <- 1.0e-7
+  gamma <- 0.9; delta <- 1.2
+  mb    <- m; t_out   <- abs(t)
+  istep <- 0; t_new   <- 0
+  t_now <- 0; s_error <- 0
+  rndoff <-  anorm*2.2204e-16
+  
+  k1 <- 2; xm <- 1/m; normv <- sqrt(sum(v)^2); beta <- normv
+  fact <- (((m+1)/exp(1))^(m+1))*sqrt(2*pi*(m+1))
+  t_new <- (1/anorm)*((fact*tol)/(4*beta*anorm))^xm
+  s <- 10^(floor(log10(t_new))-1); t_new <- ceiling(t_new/s)*s
+  sgn <- sign(t); istep <- 0
+  tlist <- NULL
+  w <- v
+  hump <- normv
+  count1 <- 0
+  
+  # I think I need to initialize these since they are vectors
+  dupgenomesavgMat <- NA
+  PIlowerMat <- NA
+  PIupperMat <- NA
+  
+  while (t_now < t_out){
+    ####################################################################
+    #TLS Main modifications come in here.
+    tlist <- c(tlist, t_now)
+    count1 <- count1+1
+    # calculate proportion duplicated conditional on wt not fixing
+    pn <- rep(0,2*N)#pn(i) stores conditional probably of haveing i haploid genomes with a duplicate at time t_now (each individual has two haploid genomes)
+    if (w[1] > 0){
+      w <- w/(1-w[1])
+      w[1] <- 0
+    }
+    pnsummer <- 0
+    pnflag <- 0
+    for (i in 1:(N+1)){
+      for (j in 1:(N+2-i)){
+        if (i!=1 || j != 1){
+          pnsummer <- pnsummer+w[Pos[i,j]]
+          pn[2*(i-1)+(j-1)] <- pn[2*(i-1)+(j-1)] + w[Pos[i,j]]
+          if (pnsummer == 1){
+            pnflag <- 1
+            break
+          }
+        }
+        if (pnflag ==1){
+          break
+        }
+      }
+      if (pnflag == 1){
+        break
+      }
+    }
+    dupgenomesavg <- pn%*%(1:(2*N))/(2*N) #Stores the expected number of haploid genomes with a duplicate at time t_now
+    
+    #Now calculate the 95# prediction interval on the proportion of
+    #duplicate haplotypes
+    
+    cumsumpn <- cumsum(pn)
+    PIlower <- min(which(cumsumpn>=0.025,1))/(2*N)  # something is wrong here with larger N
+    PIupper <- min(which(cumsumpn>=0.975,1))/(2*N)
+    dupgenomesavgMat[count1] <- dupgenomesavg
+    PIlowerMat[count1] <- PIlower
+    PIupperMat[count1] <- PIupper
+    
+    
+    #################################################
+    #TLS - Now back to standard mexpv as implemented in expokit.
+    
+    istep <- istep + 1
+    t_step <- min( t_out-t_now,t_new )
+    V <- matrix(0, nrow=n,ncol=(m+1))
+    H <- matrix(0, nrow=(m+2),ncol=(m+2))
+    
+    V[,1] <- as.double((1/beta)*as.matrix(w)) # this line seems to cause problems with bigger N
+    for (j in 1:m){
+      p <- A%*%V[,j]
+      for (i in 1:j){
+        H[i,j] <- as.double((V[,i]) %*% p)
+        p <- p-H[i,j]*V[,i]
+      }
+      s <- norm(p, "f")   # Matlab defaults to Frobenius norm, make sure this is ok though since p is a vector
+      if (s < btol){
+        k1 <- 0
+        mb <- j
+        t_step <- t_out-t_now
+        break
+      }
+      H[j+1,j] <- s
+      V[,j+1] <- as.double((1/s)*p)
+    }
+    if (k1 != 0){
+      H[m+2,m+1] <- 1
+      avnorm <- norm(A%*%V[,m+1], "f")  # 4/29/21 I think this is right but check closer
+    }
+    ireject <- 0
+    while (ireject <= mxrej){
+      mx <- mb + k1
+      F <- expm(sgn*t_step*H[1:mx,1:mx])
+      if (k1 == 0){
+        err_loc <- btol
+        break
+      } else {
+        phi1 <- abs( beta*F[m+1,1] )
+        phi2 <- abs( beta*F[m+2,1] * avnorm )
+        if (phi1 > 10*phi2){
+          err_loc <- phi2
+          xm <- 1/m
+        } else if (phi1 > phi2){
+          err_loc <- (phi1*phi2)/(phi1-phi2)
+          xm <- 1/m
+        } else {
+          err_loc <- phi1
+          xm <- 1/(m-1)
+        }
+      }
+      if (err_loc <= delta * t_step*tol){
+        break
+      } else {
+        t_step <- gamma * t_step * (t_step*tol/err_loc)^xm
+        s <- 10^(floor(log10(t_step))-1)
+        t_step <- ceiling(t_step/s) * s
+        if (ireject == mxrej){
+          error('The requested tolerance is too high.')
+        }
+        ireject <- ireject + 1
+      }
+    }
+    mx <- mb + max( c(0,(k1-1) ))
+    w <- V[,(1:mx)]%*%(beta*F[1:mx,1])   # check this 4/29/21
+    beta <- norm( w , "f")
+    hump <- max(hump,beta)
+    
+    ineg <- 0
+    for (i in 1:n){
+      if (w[i] < 0){
+        w[i] <- 0
+        ineg <- ineg + 1
+      }
+    }
+    wnorm <- norm(w)
+    if (ineg > 0){
+      w <- (1/wnorm)*w
+    } 
+    roundoff <- abs(1-wnorm)/n   # this was 1.0d0, no idea what that means
+    
+    t_now <- t_now + t_step
+    t_new <- gamma * t_step * (t_step*tol/err_loc)^xm
+    s <- 10^(floor(log10(t_new))-1)
+    t_new <- ceiling(t_new/s) * s
+    
+    err_loc <- max(err_loc,roundoff)
+    err_loc <- max(err_loc,rndoff)
+    s_error <- s_error + err_loc
+  }
+  err <- s_error
+  hump <- hump / normv
+  
+  return(list(dupgenomesavgMat,PIlowerMat,PIupperMat,tlist,w))
+}
+
