c8774d8607b8fe8fc2751172a423514ab48ba57e
peterbchi <peter.chi@villanova.edu>
2021-05-07 13:25:13 -0400

    added summary plotting function

---
+++ R/CNVSelect_plot.R
@@ -0,0 +1,36 @@
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+#' Function to create summary plot of data and confidence intervals
+#'
+#' This function takes output from CNVSelect_test and creates a summary plot
+#'
+#'
+#' @param test_out object that contains output from CNVSelect_test
+#' @keywords phylogeny, CNV, neutral model
+#' @export
+#' @examples
+#' CNVSelect_summary(test_out)
+
+CNVSelect_plot <- function(test_out){
+  y_max <- max(c(test_out$freqs, test_out$CIupper)) * 1.3
+  x_max <- max(test_out$dS) * 1.3
+  plot(test_out$freqs ~ as.vector(test_out$dS),
+       xlim=c(0,x_max),
+       ylim=c(0,y_max),
+       xlab="dS",
+       ylab="frequency",
+       main="95% Confidence Intervals and data points",
+       pch=19)
+
+  # add confidence intervals
+  n.seq <- length(test_out$freqs)
+  CI_lw <- x_max/50
+
+  for(i in 1:n.seq){
+    lines(x=c(test_out$dS[i], test_out$dS[i]), y=c(test_out$CIlower[i], test_out$CIupper[i]))
+    lines(x=c(test_out$dS[i]-CI_lw, test_out$dS[i]+CI_lw), y=c(test_out$CIlower[i], test_out$CIlower[i]))
+    lines(x=c(test_out$dS[i]-CI_lw, test_out$dS[i]+CI_lw), y=c(test_out$CIupper[i], test_out$CIupper[i]))
+  }
+}
